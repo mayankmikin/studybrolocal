f6fe791b2d181d1d68b7afb106396d18dadbf3dd
MAYANK-PC\Mayank Verma <mayank.verma@nviz.com>
2017-06-26 17:57:31 +0530

    proper null check condition

--- studybro/services/ValidationService.js
+++ studybro/services/ValidationService.js
@@ -2,7 +2,7 @@ angular.module('hmModule').service('ValidationServcie', function()
 {
     this.nullcheck = function (x) 
     {
-        if(x!=null)
+        if((x!=null)||(x!='')||(x!=""))
         {
         	return true;
         }
